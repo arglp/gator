15ac142bb3435fd3ef36d2f234b9682c33abcf82
arglp <leopold.prueller@gmx.at>
2025-09-03 23:14:31 +0200

    added cli.go

--- main.go
+++ main.go
@@ -11,16 +11,5 @@ func main() {
 	if err != nil {
 		log.Fatalf("error reading config: %v", err)
 	}
-	fmt.Printf("Read config: %+v\n", cfg)
-
-	err = cfg.SetUser("arglp")
-	if err != nil {
-		log.Fatalf("couldn't set current user: %v", err)
-	}
-
-	cfg, err = config.Read()
-	if err != nil {
-		log.Fatalf("error reading config: %v", err)
-	}
-	fmt.Printf("Read config again: %+v\n", cfg)
+	s := state{cfg: *cfg}
 }
